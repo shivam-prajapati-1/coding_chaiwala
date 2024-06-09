8903721bc02c0de6e2e8df0452b6d1be794c6a7f
shivam-prajapati-1 <163870678+shivam-prajapati-1@users.noreply.github.com>
2024-06-09 14:21:41 +0530

    calculator

---
+++ Calculator/script.js
@@ -0,0 +1,12 @@
+let result=document.getElementById("result");
+function getResult(value){
+    if(value==="="){
+        result.value=eval(result.value)
+    }
+    else{
+        result.value+=value;
+    }
+}
+function clearResult(){
+    result.value="";
+}
